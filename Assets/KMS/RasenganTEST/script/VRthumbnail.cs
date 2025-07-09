d81c865ac4c3da41816a12e27acd5e8923acf8ef
minmin4045 <kimminseok4045@gmail.com>
2025-07-09 15:47:11 +0900

    라이팅

--- Assets/KMS/RasenganTEST/script/VRthumbnail.cs
+++ Assets/KMS/RasenganTEST/script/VRthumbnail.cs
@@ -6,6 +6,6 @@ public class SceneLoader : MonoBehaviour
     void Start()
     {
         // "VirtualScene" 씬을 Additive로 불러오기
-        SceneManager.LoadScene("VR_DodAI", LoadSceneMode.Additive);
+        SceneManager.LoadScene("VR_DodAI_KMS", LoadSceneMode.Additive);
     }
 }
